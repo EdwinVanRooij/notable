07e9c3170f16c0845e1b343f5f09a6da85405a62
Edwin <edwinvrooij@gmail.com>
2016-04-27 15:48:47 +0200

    Added all changes at 2016-04-27_15.48 - (automated commit from batch)

--- update.bat
+++ update.bat
@@ -10,11 +10,11 @@ set min=%time:~-8,2%
 
 rem Set the date string for commit message
 echo "Initializing strings..."
-set datestring="%year%-%month%-%day%_%hour%.%min%"
+set datestring=""
 
 rem Set the commit message
 set commit_message="Added all changes"
-set final_message="%commit_message% at %datestring% - (automated commit from batch)"
+set final_message="%commit_message% at %datestring% "
 
 rem Add all files to the git head
 echo "Adding all files to git head..."
@@ -22,7 +22,7 @@ git add .
 
 rem Commit all changes
 echo "Committing changes..."
-git commit -m "test commit msg %min%"
+git commit -m "Added all changes at %year%-%month%-%day%_%hour%.%min% - (automated commit from batch)"
 
 rem Push to remote git repo
 echo 'Pushing changes...'
